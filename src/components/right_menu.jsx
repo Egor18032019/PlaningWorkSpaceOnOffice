a0451009dfb5b64c6fd419624795257399c9f3e6
Egor <goro4@mail.ru>
2020-11-26 16:38:27 +0500

    сделал перетасквание правого popupa

--- src/components/right_menu.jsx
+++ src/components/right_menu.jsx
@@ -1,29 +1,90 @@
-import React, {PureComponent, createRef} from "react";
+import React, {useRef} from "react";
 import PropTypes from "prop-types";
 
 import MenuItem from "./menu-item.jsx";
 
 // Меню ФИО
 
-class RightMenu extends PureComponent {
-  constructor(props) {
-    super(props);
-    this.textRef = createRef();
-    this.onChange = this.onChange.bind(this);
+const RightMenu = (props) => {
+  const textRef = useRef();
+
+  function onChange() {
+    const {handleFilterMenu} = props;
+    handleFilterMenu(textRef);
   }
 
-  onChange() {
-    const {handleFilterMenu} = this.props;
-    handleFilterMenu(this.textRef);
-  }
+  const dragAndDrop = () => {
 
-  render() {
-    const {places, onPinClick, filterPlaces} = this.props;
+    const pinRef = useRef(null);
+
+    const handleDown = (e) => {
+
+      e.preventDefault();
+      const escFunction = (evt) => {
+        if (evt.keyCode === 27) {
+          setupDialogElement.onmousemove = null;
+          pinRef.current.onmouseup = null;
+          document.removeEventListener(`keydown`, escFunction, false);
+        }
+      };
+      document.addEventListener(`keydown`, escFunction, false);
+
+      const setupDialogElement = document.querySelector(`main`);
+      let rect = setupDialogElement.getBoundingClientRect();
+      pinRef.current.style.cursor = `pointer`;
+      let pinWidth = pinRef.current.offsetWidth;
+      moveAt(e);
+
+      function moveAt(evt) {
+        evt.preventDefault();
+        let coordX = evt.clientX - rect.left;
+
+
+        let coordY = evt.clientY - rect.top;
+        if ((coordY < 25) || (coordY > 1100)) {
+          setupDialogElement.onmousemove = null;
+          pinRef.current.onmouseup = null;
+          coordY = Math.max(25, Math.min(coordY, 1100));
+        }
+
+        if ((coordX < -250) || (coordX > 1400)) {
+          setupDialogElement.onmousemove = null;
+          pinRef.current.onmouseup = null;
+          coordX = Math.max(-250, Math.min(coordX, 1400));
+        }
+        coordX = Math.max(-250, Math.min(coordX, 1400));
+
+        let coordinateY = coordY - 35 + `px`;
+        // let coordinateX = coordX + pinWidth / 2 + `px`;
+        let coordinateX = coordX + pinWidth + `px`;
+
+        pinRef.current.style.top = coordinateY;
+        pinRef.current.style.left = coordinateX;
+      }
+
+      // 3, перемещать в пределах выбранной области
+      setupDialogElement.onmousemove = function (ev) {
+        moveAt(ev);
+      };
+
+      // 4. отследить окончание переноса
+      pinRef.current.onmouseup = function (upEvt) {
+        console.log(`onmouseup`);
+        upEvt.preventDefault();
+        setupDialogElement.onmousemove = null;
+        pinRef.current.onmouseup = null;
+      };
+      // 5. Чтоб не обрабатывался как картинка браузером
+      pinRef.current.ondragstart = function () {
+        return false;
+      };
+    };
+
     return (
-      <article className="menu">
+      <article className="menu menu-drag" ref={pinRef}
+        onMouseDown={handleDown}>
         <p className="menu-text">Список ФИО И № р.м.</p>
-
-        <input type="text" placeholder="Искать здесь" name="t" ref={this.textRef} onChange={this.onChange} />
+        <input type="text" placeholder="Искать здесь" name="search" ref={textRef} onChange={onChange} />
         <ul className="menu-list">
           {filterPlaces.map(
               (place) => {
@@ -39,15 +100,11 @@ class RightMenu extends PureComponent {
         <b>Итого {places.length} рабочих мест</b>
       </article>
     );
-  }
-  componentDidMount() {
-    // console.log(`componentDidMount Right`);
+  };
+  const {places, onPinClick, filterPlaces} = props;
+  return (dragAndDrop());
+};
 
-  }
-  componentDidUpdate() {
-    // console.log(`componentDidUpdate Right`);
-  }
-}
 RightMenu.propTypes = {
   places: PropTypes.array.isRequired,
   filterPlaces: PropTypes.array.isRequired || null,
