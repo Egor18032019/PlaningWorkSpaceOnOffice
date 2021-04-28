2e4a092029687f56a92842eb75d10acd05e49f58
Egor18032019 <goro4@mail.ru>
2021-04-28 10:35:28 +0500

    мелкие правки

--- src/components/pin.jsx
+++ src/components/pin.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const Pin = (props) => {
-  const {place, activePlace} = props;
+  const { place, activePlace } = props;
 
   function onPinClickForPopup() {
     // ожидаю что при клике на другой пин и передачи  onPinClick(null);
@@ -11,7 +11,7 @@ const Pin = (props) => {
     // ожидания не  оправдываються
     // если удалять popup.remove(); то он всё равно не работает
     // const popup = document.querySelector(`.popup-form`);
-    const {onPinClick} = props;
+    const { onPinClick } = props;
     onPinClick(place);
   }
 
@@ -20,17 +20,17 @@ const Pin = (props) => {
     if (place === activePlace) {
       classForActive = `active`;
     }
-    const {coordinateX, coordinateY, id} = place;
+    const { coordinateX, coordinateY, id } = place;
     /**
      * Ширина пина
      */
     let pinWidth = 20;
     // высота пина
     let pinHeight = 20;
-    let coordinateXcurrent = coordinateX - 15 + pinWidth + `px`;
-    let coordinateYcurrent = coordinateY + 15 + pinHeight + `px`;
+    let coordinateXcurrent = coordinateX + pinWidth + `px`;
+    let coordinateYcurrent = coordinateY + pinHeight + `px`;
     return (
-      <button type="button" className={`map__pin` + ` ` + classForActive} style={{left: coordinateXcurrent, top: coordinateYcurrent}}
+      <button type="button" className={`map__pin` + ` ` + classForActive} style={{ left: coordinateXcurrent, top: coordinateYcurrent }}
         onClick={onPinClickForPopup}>
         {/* если картинку то не видно номера места. Подумать про другие варианты */}
         {/* <img src="img/avatars/user02.png" width="20" height="20" draggable="false"
